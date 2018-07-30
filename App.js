a726e3a7424f0ff63780c77e92503f688a0a40f2
John O'Sullivan <josullivan1@luc.edu>
2018-07-29 19:47:46 -0500

    Debugging storage component

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import NavBar, { NavButton, NavButtonText, NavTitle } from 'react-native-nav';
 
 import './global';
 
@@ -9,16 +10,46 @@ const web3 = new Web3(
   new Web3.providers.HttpProvider('https://mainnet.infura.io/'),
 );
 
+
+import Storage from 'react-native-storage';
+import { AsyncStorage } from 'react-native';
+
+var storage = new Storage({
+	// maximum capacity, default 1000
+	size: 1000,
+
+	// Use AsyncStorage for RN, or window.localStorage for web.
+	// If not set, data would be lost after reload.
+	storageBackend: AsyncStorage,
+
+	// expire time, default 1 day(1000 * 3600 * 24 milliseconds).
+	// can be null, which means never expire.
+	defaultExpires: 1000 * 3600 * 24,
+
+	// cache data in the memory. default is true.
+	enableCache: true,
+
+	// if data was not found in storage or expired,
+	// the corresponding sync method will be invoked and return
+	// the latest data.
+	sync : {
+		// we'll talk about the details later.
+	}
+})
+
 export default class App extends React.Component {
 
   onPressLearnMore = () => {
-    web3.eth.getBlock('latest').then(console.log);
+    web3.eth.getBlock('latest').then((block) => {
+      console.log("Block: ", block.number);
+    });
   }
 
   render() {
     // printing on the console the latest ethereum block
     return (
       <View style={styles.container}>
+
       <Button
   onPress={this.onPressLearnMore}
   title="Learn More"
