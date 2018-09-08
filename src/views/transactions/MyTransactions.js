7cd025d25bb6e388771b5ed6eb4457699e036f00
John O'Sullivan <josullivan1@luc.edu>
2018-09-08 18:30:01 -0500

    UI Updates

--- src/views/transactions/MyTransactions.js
+++ src/views/transactions/MyTransactions.js
@@ -6,7 +6,8 @@ import {
   TextInput,
   TouchableOpacity,
   ActivityIndicator,
-  Image
+  Image,
+  ScrollView
 } from 'react-native';
 
 import { 
@@ -16,6 +17,8 @@ import {
   CheckBox, 
   Icon  
 } from 'react-native-elements';
+
+import { Col, Row, Grid } from 'react-native-easy-grid';
 
 const styles = StyleSheet.create({
     container: {
@@ -27,6 +30,29 @@ const styles = StyleSheet.create({
 });
 
 export default class MyTransactions extends Component {
+
+    state = {
+        txs: [
+           { 
+               "txHash": "0x392c01d11d692908c5d899fa4ddb091e2f641524163c21f1c74fc0229b565e86",
+               "gasUsed": "21000",
+               "mineDate": "Sep-08-2018 10:43:55 PM +UTC",
+               "blockNumber": "6296691",
+               "toAddress": "0x689c56aef474df92d44a1b70850f808488f9769c",
+               "id": 2849273,
+               "status": 1
+           },
+           { 
+                "txHash": "0x392c01d11d692908c5d899fa4ddb091e2f641524163c21f1c74fc0229b565e86",
+                "gasUsed": "21000",
+                "mineDate": "Sep-08-2018 10:43:55 PM +UTC",
+                "blockNumber": "6296691",
+                "toAddress": "0x689c56aef474df92d44a1b70850f808488f9769c",
+                "id": 284935273,
+                "status": 1
+            }
+        ]
+    }
 
     render() {
         return (
@@ -58,10 +84,54 @@ export default class MyTransactions extends Component {
                         />
                         </TouchableOpacity>
                     }
-                />
-                <View>
-                    { /*  */ }
-                </View>
+                />                    
+                <ScrollView style = {{ paddingTop: 4 }}>
+                {
+                  this.state.txs.map((item, index) => (
+                     <View key = {item.id}
+                        style = {{
+                            flexDirection: 'row',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                            padding: 10,
+                            margin: 2,
+                            backgroundColor: '#eee'
+                    }}>
+
+                    <Grid>
+
+                        <Row>
+                        <Text numberOfLines={1} ellipsizeMode='middle' style = {{
+                        fontWeight: '600',
+                        color: (item.status ? 'green' : 'red')
+                        }}>TxHash: {item.txHash}</Text>
+                        </Row>
+
+                        <Row>
+                        <Col>
+                        <Text style = {{
+                        fontWeight: '500',
+                        }}>Block #: {item.blockNumber}</Text>
+                        </Col>
+                        <Col>
+                        <Text style = {{
+                        fontWeight: '500',
+                        }}>Gas Used: {item.gasUsed}</Text>
+                        </Col>
+                        </Row>
+
+                        <Row>
+                        <Text numberOfLines={1} ellipsizeMode='middle' style = {{
+                        fontWeight: '400',
+                        }}>Address: {item.toAddress}</Text>
+                        </Row>
+
+                    </Grid>
+
+                     </View>
+                  ))
+                }
+                </ScrollView>
             </View>
         )
     }
